c8c6ee01e34beb9df7cce07fa81a4297b2e3db32
famew1 <famew1@student.monash.edu>
2013-09-13 11:02:43 +1000

    Update ApplicantsController.php

--- Controller/ApplicantsController.php
+++ Controller/ApplicantsController.php
@@ -56,7 +56,7 @@ class ApplicantsController extends AppController {
 			}
 		}
 		$clientcases = $this->Applicant->Clientcase->find('list');
-		$archives = $this->Applicant->Archive->find('list');
+		$archives = $this->Applicant->Archive->find('list', array('fields' => array('Archive.id', 'Archive.archive_name'), 'order' => 'archive_name ASC'));
 		$this->set(compact('clientcases', 'archives'));
 	}
 
