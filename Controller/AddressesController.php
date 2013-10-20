54cc041b7ec8c3d992d44b03957b682946567a07
ejhon1 <lizziness@gmail.com>
2013-10-20 21:18:10 +1100

    Update AddressesController.php

--- Controller/AddressesController.php
+++ Controller/AddressesController.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('AppController', 'Controller');
+App::uses('CakeTime', 'Utility');
 /**
  * Addresses Controller
  *
@@ -46,19 +47,23 @@ class AddressesController extends AppController {
  * @return void
  */
 	public function add($id = null) {
-		$this->request->data['Address']['applicant_id'] = $id;
+        $this->loadModel('Clientcase');
+        $this->loadModel('Country');
+        $this->request->data['Address']['applicant_id'] = $id;
+        $clientcase = $this->Clientcase->find('first', array('conditions' => array('Clientcase.applicant_id' => $id)));
 		if ($this->request->is('post')) {
 			$this->Address->create();
 			if ($this->Address->save($this->request->data)) {
 				$this->Session->setFlash(__('The address has been saved', null),'default', array('class' => 'alert-success'));
-				return $this->redirect(array('action' => 'index'));
+				return $this->redirect(array('controller' => 'clientcases', 'action' => 'view', $clientcase['Clientcase']['id']));
 			} else {
 				$this->Session->setFlash(__('The address could not be saved. Please, try again.', null),'default', array('class' => 'alert-danger'));
 			}
 		}
-		$applicants = $this->Address->Applicant->find('list');
-		$countries = $this->Address->Country->find('list', array('fields' => array('Country.id', 'Country.country_name')));
-		$this->set(compact('applicants', 'countries'));
+		//$applicants = $this->Address->Applicant->find('list');
+		$countries = $this->Country->find('list');
+        $this->set(compact('countries'));
+		//$this->set(compact('applicants', 'countries'));
 	}
 
 /**
@@ -69,24 +74,38 @@ class AddressesController extends AppController {
  * @return void
  */
 	public function edit($id = null) {
+        //$this->loadModel('Clientcase');
+        $this->loadModel('Applicant');
+        $this->loadModel('Address');
+        $applicant = $this->Applicant->query("SELECT distinct *
+         FROM applicants AS Applicant, addresses AS Address
+         WHERE Address.id = ".$id." AND Address.applicant_id = Applicant.id;");
+        $this->request->data['Address']['applicant_id'] = $applicant['Address']['applicant_id'];
+
 		if (!$this->Address->exists($id)) {
 			throw new NotFoundException(__('Invalid address'));
 		}
 		if ($this->request->is('post') || $this->request->is('put')) {
+
+            $this->Address->create();
 			if ($this->Address->save($this->request->data)) {
 				$this->Session->setFlash(__('The address has been saved', null),'default', array('class' => 'alert-success'));
-				
-				return $this->redirect(array('controller' => 'clientcases', 'action' => 'view', $id));
+
+                $this->Address->set('id', $id);
+                $this->request->data['Address']['date_changed'] = date('y-m-d');
+                $this->Address->save($this->request->data);
+
+				return $this->redirect(array('controller' => 'clientcases', 'action' => 'view', $applicant['Applicant']['clientcase_id']));
 			} else {
-				$this->Session->setFlash(__('The address could not be saved. Please, try again.', null),'default', array('class' => 'alert-danger'));
+				$this->Session->setFlash(__('The address could not be saved. Please try again.', null),'default', array('class' => 'alert-danger'));
 			}
 		} else {
 			$options = array('conditions' => array('Address.' . $this->Address->primaryKey => $id));
 			$this->request->data = $this->Address->find('first', $options);
 		}
-		$applicants = $this->Address->Applicant->find('list');
+		//$applicants = $this->Address->Applicant->find('list');
 		$countries = $this->Address->Country->find('list', array('fields' => array('Country.id', 'Country.country_name')));
-		$this->set(compact('applicants', 'countries'));
+		$this->set(compact('applicant', 'countries'));
 	}
 
 /**
