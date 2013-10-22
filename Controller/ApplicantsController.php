3905ea23bd2a7961d2be4dd6a528b9b257bc9d75
ejhon1 <lizziness@gmail.com>
2013-10-22 17:48:25 +1100

    Update ApplicantsController.php

--- Controller/ApplicantsController.php
+++ Controller/ApplicantsController.php
@@ -48,13 +48,14 @@ class ApplicantsController extends AppController {
 	public function add($id = null) {
 		$this->request->data['Applicant']['clientcase_id'] = $id;
 		if ($this->request->is('post')) {
-			$this->Applicant->create();
+            $this->request->data['Applicant']['birthdate'] = date('Y-m-d', strtotime(str_replace('/', '-', $this->request->data['Applicant']['birthdate'])));
+
+            $this->Applicant->create();
 			if ($this->Applicant->save($this->request->data)) {
 				$this->Session->setFlash(__('The applicant has been saved', null),'default', array('class' => 'alert-success'));
 				return $this->redirect(array('controller' => 'clientcases', 'action' => 'view', $id));
 			} else {
-				$this->Session->setFlash(__('The applicant could not be saved. Please, try again.', null),'default', array('class' => 'alert-danger'));
-				return $this->redirect(array('controller' => 'clientcases', 'action' => 'view', $id));
+				$this->Session->setFlash(__('The applicant could not be saved. Please try again.', null),'default', array('class' => 'alert-danger'));
 			}
 		}
 		$clientcases = $this->Applicant->Clientcase->find('list');
@@ -72,8 +73,13 @@ class ApplicantsController extends AppController {
 		if (!$this->Applicant->exists($id)) {
 			throw new NotFoundException(__('Invalid applicant'));
 		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Applicant->save($this->request->data)) {
+        $applicant = $this->Applicant->findByid($id);
+        $test = date('d/m/Y', strtotime($applicant['Applicant']['birthdate']));
+
+        if ($this->request->is('post') || $this->request->is('put')) {
+            $this->request->data['Applicant']['birthdate'] = date('Y-m-d', strtotime(str_replace('/', '-', $this->request->data['Applicant']['date'])));
+
+            if ($this->Applicant->save($this->request->data)) {
 				$this->Session->setFlash(__('The applicant has been saved', null),'default', array('class' => 'alert-success'));
 				
 				return $this->redirect(array('controller' => 'clientcases', 'action' => 'view', $this->request->data['Applicant']['clientcase_id']));
@@ -83,7 +89,7 @@ class ApplicantsController extends AppController {
 		}
         $options = array('conditions' => array('Applicant.' . $this->Applicant->primaryKey => $id));
         $this->request->data = $this->Applicant->find('first', $options);
-		$this->set(compact('id'));
+		$this->set(compact('id', 'test'));
 	}
 
 /**
