1a06a78e6461efd33b13c9f678857554b8d1e3b4
famew1 <famew1@student.monash.edu>
2013-10-22 19:19:10 +1100

    Update Documenttype.php

--- Model/Documenttype.php
+++ Model/Documenttype.php
@@ -7,7 +7,43 @@ App::uses('AppModel', 'Model');
  */
 class Documenttype extends AppModel {
 
-
+	public $validate = array(
+		'category' => array(
+            'notempty' => array(
+			'rule' => array(
+            	'notempty'),
+                'message' => 'Please enter your first name'
+		),
+			'validCategory' => array(
+                'rule' => '/^[a-zA-Z\s.\-]+$/',
+				'message' => 'Category can only contains letters',
+            )
+        ),
+		'type' => array(
+            'notempty' => array(
+				'rule' => array(
+            		'notempty'),
+                	'message' => 'Please enter document type'
+			),
+			'validType' => array(
+                'rule' => '/^[a-zA-Z\s.\-]+$/',
+				'message' => 'Document type can only contains letters',
+            	),
+			),
+		'code' => array(
+            'notempty' => array(
+				'rule' => array(
+            		'notempty'),
+                	'message' => 'Please enter document code'
+			),
+			'validCode' => array(
+                'rule' => '/^[a-zA-Z\s.\-]+$/',
+				'message' => 'Document code can only contains letters',
+				'allowEmpty' => false
+            ),
+        ),
+	
+	);
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 /**
