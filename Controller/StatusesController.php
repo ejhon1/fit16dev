85ce0e47cee43f0c1c0288182ab7284e98e8fe93
ejhon1 <lizziness@gmail.com>
2013-12-16 15:09:17 +1100

    Update StatusesController.php

--- Controller/StatusesController.php
+++ Controller/StatusesController.php
@@ -16,31 +16,6 @@ class StatusesController extends AppController {
 	public $components = array('Paginator');
 
 /**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->Status->recursive = 0;
-		$this->set('statuses', $this->Paginator->paginate());
-	}
-
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->Status->exists($id)) {
-			throw new NotFoundException(__('Invalid status'));
-		}
-		$options = array('conditions' => array('Status.' . $this->Status->primaryKey => $id));
-		$this->set('status', $this->Status->find('first', $options));
-	}
-
-/**
  * add method
  *
  * @return void
@@ -73,7 +48,7 @@ class StatusesController extends AppController {
 				$this->Session->setFlash(__('The status has been saved', null),'default', array('class' => 'alert-success'));
                 return $this->redirect('../management');
 			} else {
-				$this->Session->setFlash(__('The status could not be saved. Please, try again.', null),'default', array('class' => 'alert-danger'));
+				$this->Session->setFlash(__('The status could not be saved. Please try again.', null),'default', array('class' => 'alert-danger'));
 			}
 		} else {
 			$options = array('conditions' => array('Status.' . $this->Status->primaryKey => $id));
